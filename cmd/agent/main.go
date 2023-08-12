dce10b1ebea685dc0791c5899dfb872eb9b7b56f
PolyaevMA <polyaev.maks@ya.ru>
2023-08-12 04:02:42 +0300

    fix(iter5): undo context

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"context"
-	"github.com/MaximPolyaev/go-metrics/internal/agent/httpclient"
-	"github.com/MaximPolyaev/go-metrics/internal/agent/metric"
-	"github.com/MaximPolyaev/go-metrics/internal/agent/services/stats"
-	"github.com/MaximPolyaev/go-metrics/internal/config"
 	"log"
 	"sync"
+	"time"
+
+	"github.com/MaximPolyaev/go-metrics/internal/agent/httpclient"
+	"github.com/MaximPolyaev/go-metrics/internal/agent/metric"
+	"github.com/MaximPolyaev/go-metrics/internal/config"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatalln(err)
-	}
+	run()
 }
 
-func run() (err error) {
+func run() {
 	cfg := config.NewBaseConfig()
 	if err := cfg.Parse(); err != nil {
 		log.Fatalln(err)
@@ -26,25 +24,30 @@ func run() (err error) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	ctx, cancel := context.WithCancel(context.Background())
 	httpClient := httpclient.NewHTTPClient(*cfg.Addr)
-	statsService := stats.New(ctx, httpClient)
 
-	go statsService.Pool(*cfg.PollInterval, &mStats)
+	poolInterval := time.NewTicker(time.Duration(*cfg.PollInterval) * time.Second)
+	reportInterval := time.NewTicker(time.Duration(*cfg.ReportInterval) * time.Second)
 
 	go func() {
 		for {
-			err = statsService.Report(*cfg.ReportInterval, &mStats)
+			select {
+			case <-poolInterval.C:
+				metric.ReadStats(&mStats)
+			}
+		}
+	}()
 
-			if err != nil {
-				wg.Done()
-				break
+	go func() {
+		for {
+			select {
+			case <-reportInterval.C:
+				if err := httpClient.UpdateMetrics(&mStats); err != nil {
+					log.Fatalln(err)
+				}
 			}
 		}
 	}()
 
 	wg.Wait()
-	cancel()
-
-	return
 }
