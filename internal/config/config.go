e8bf1e1c1df571995a64e5b10fb342266f53dce4
PolyaevMA <polyaev.maks@ya.ru>
2023-09-12 22:08:27 +0300

    feat(iter10): update cfgs

--- internal/config/config.go
+++ internal/config/config.go
@@ -70,7 +70,7 @@ func (cfg *AddressConfig) EnvParse() error {
 func (cfg *AddressConfig) ConfigureFlags() {
 	if cfg.Addr == nil {
 		cfg.Addr = new(string)
-		flag.StringVar(cfg.Addr, "a", ":8080", "http server addr")
+		flag.StringVar(cfg.Addr, "a", "http://localhost:8080", "http server addr")
 	}
 }
 
